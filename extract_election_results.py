30f4f6711c948552b3e4e945c21c4e31e4d6d9a8
sheldonrampton <sheldonmrampton@gmail.com>
2025-01-01 16:17:32 -0600

    clean up anomalies, standardize names

--- extract_election_results.py
+++ extract_election_results.py
@@ -14,6 +14,22 @@ def clean_candidate_name(name):
     return name.strip()
 
 
+def is_excluded_race(race_name):
+    exclusions = [
+        "Court of Appeals Judge",
+        "Justice of the Supreme Court",
+        # "Multi-jurisdictional Judge",
+        "President of the United States",
+        "Presidential Preference Vote",
+        "State Superintendent",
+        "State Senator"
+    ]
+    for item in exclusions:
+        if race_name.startswith(item):
+            return True
+    return False
+
+
 def strip_up_to_nonpartisan(input_text):
     target_string = "(NONPARTISAN)"
     lines = input_text.strip().split('\n')
@@ -121,6 +137,44 @@ def parse_race_name(race_name):
         office = "School Board Member"
         jurisdiction = f"{parts[1].strip()}"
         return {"office": office, "jurisdiction": jurisdiction}
+    elif race_name.startswith("County Supervisor"):
+        parts = race_name.split("District", 1)
+        office = f"{parts[0].strip()} District {parts[1].strip()}"
+        jurisdiction = "Columbia County"
+        return {"office": office, "jurisdiction": jurisdiction}
+    elif race_name.startswith("Circuit Court Judge Branch"):
+        parts = race_name.split("Columbia County", 1)
+        office = f"{parts[0].strip()}"
+        jurisdiction = "Columbia County Circuit Court"
+        return {"office": office, "jurisdiction": jurisdiction}
+    elif race_name.startswith("Circuit Court Judge"):
+        office = "Circuit Court Judge"
+        jurisdiction = "Columbia County Circuit Court"
+        return {"office": office, "jurisdiction": jurisdiction}
+    elif race_name.startswith("Sanitary District Commissioner"):
+        parts = race_name.split("Sanitary District Commissioner", 1)
+        office = "Sanitary District Commissioner"
+        jurisdiction = f"{parts[1].strip()}"
+        return {"office": office, "jurisdiction": jurisdiction}
+    elif race_name.startswith("Municipal Judge") and (race_name.endswith("Portage") or race_name.endswith("Eastern")):
+        parts = race_name.split("Municipal Judge", 1)
+        office = "Multi-jurisdictional Judge"
+        jurisdiction = f"{parts[1].strip()} Multi-jurisdiction"
+        return {"office": office, "jurisdiction": jurisdiction}
+    elif race_name.startswith("Multi-jurisdictional Judge"):
+        parts = race_name.split("Multi-jurisdictional Judge", 1)
+        office = "Multi-jurisdictional Judge"
+        jurisdiction = f"{parts[1].strip()} Multi-jurisdiction"
+        return {"office": office, "jurisdiction": jurisdiction}
+    elif "Alderperson City of Lodi" == race_name:
+        office = "Alderperson"
+        jurisdiction = "City of Lodi"
+        return {"office": office, "jurisdiction": jurisdiction}
+    elif "Alderperson" in race_name:
+        parts = race_name.split("Alderperson", 1)
+        office = f"Alderperson {parts[1].strip()}"
+        jurisdiction = f"{parts[0].strip()}"
+        return {"office": office, "jurisdiction": jurisdiction}
     else:
         # Iterate over jurisdiction types to find a match
         for jurisdiction_type in jurisdiction_types:
@@ -129,7 +183,6 @@ def parse_race_name(race_name):
                 office = parts[0].strip()
                 jurisdiction = f"{jurisdiction_type} {parts[1].strip()}"
                 return {"office": office, "jurisdiction": jurisdiction}
-
     # If no jurisdiction type is found, return the whole string as the office
     return {"office": race_name.strip(), "jurisdiction": ""}
 
@@ -210,8 +263,8 @@ def process_election_data(input_file, output_file):
         # Append data for each candidate to the processed_data list
         for i, (candidate_name, votes_received, percent_received) in enumerate(candidates):
             elected = 1 if i < number_of_seats else 0
-            if candidate_name not in excluded_candidates:
-                race_name = fix_case(race_name, fix_all = True)
+            race_name = fix_case(race_name, fix_all = True)
+            if candidate_name not in excluded_candidates and not is_excluded_race(race_name):
                 jurisdiction = ""
                 office = ""
                 if race_name.startswith("Alderperson") and candidate_name in alders:
@@ -221,6 +274,111 @@ def process_election_data(input_file, output_file):
                     race_fields = parse_race_name(race_name)
                     jurisdiction = race_fields['jurisdiction']
                     office = race_fields['office']
+                # Standard naming convention for jurisdicitons
+                if jurisdiction in standardized_names:
+                    jurisdiction = standardized_names[jurisdiction]
+                if office in standardized_names:
+                    office = standardized_names[office]
+                processed_data.append({
+                    "Jurisdiction": jurisdiction,
+                    "Office": office,
+                    "Race Name": race_name,
+                    "Number of Seats": number_of_seats,
+                    "Candidate Name": candidate_name,
+                    "Votes Received": votes_received,
+                    "Percent Received": percent_received,
+                    "Total Votes": total_votes,
+                    "Elected": elected,
+                    "Election Date": election_date
+                })
+
+    # Write processed data to a CSV file
+    election_date = election_date.replace('/','.')
+    with open(output_file + '-' + election_date + ".csv", 'w', newline='') as csvfile:
+        fieldnames = [
+            "Jurisdiction", "Office",
+            "Race Name", "Number of Seats", "Candidate Name", "Votes Received",
+            "Percent Received", "Total Votes", "Elected", "Election Date"
+        ]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(processed_data)
+    return processed_data
+
+
+def process_2024_election_data(input_file, output_file):
+    """
+    The election report for 2024 was formatted differently, so it needs a different
+    processing function.
+    """
+
+    with open(input_file, 'r') as file:
+        raw_data = file.read()
+
+    # Replace <CR><LF> with \n for proper splitting
+    raw_data = raw_data.replace('\r\n', '\n')
+    election_date = "04/02/24"
+
+    # Split data into blocks for each race
+    race_blocks = raw_data.split('\n\n')
+
+    processed_data = []
+
+    jurisdictions = get_jurisdictions("jurisdictions.csv")
+    standardized_names = get_candidate_standard_names("standard_names.csv")
+    for block in race_blocks:
+        lines = block.strip().split('\n')
+
+        if len(lines) < 2:
+            continue
+
+        # Extract the race name and number of seats
+        race_name = lines[0].strip()
+        seats_match = re.search(r'Vote For\s+(\d+)', lines[1].strip())
+        if not seats_match:
+            print("PROBLEM WITH", lines[1])
+            print(lines)
+            continue
+        number_of_seats = int(seats_match[1])
+
+        # Process candidate lines
+        total_votes = 0
+        candidates = []
+
+        for line in lines[2:]:
+            candidate_match = re.match(r'^(.*?)\s+(\d+[,.\d+]*)$', line)
+            if candidate_match:
+                candidate_name = fix_case(clean_candidate_name(candidate_match.group(1).strip()))
+                if candidate_name in standardized_names:
+                    candidate_name = standardized_names[candidate_name]
+                votes_received = int(candidate_match.group(2).replace(',', ''))
+                candidates.append((candidate_name, votes_received))
+                total_votes += votes_received
+
+        # Sort candidates by votes received in descending order
+        candidates.sort(key=lambda x: x[1], reverse=True)
+
+        excluded_candidates = ['Write-in', 'Write-In Totals', 'Yes', 'No']
+        alders = get_alders("alders.csv")
+        # Append data for each candidate to the processed_data list
+        for i, (candidate_name, votes_received) in enumerate(candidates):
+            percent_received = round((votes_received / total_votes) * 100, 1)
+            elected = 1 if i < number_of_seats else 0
+            if candidate_name not in excluded_candidates:
+                jurisdiction = ""
+                office = ""
+                if race_name.startswith("Alderperson") and candidate_name in alders:
+                    jurisdiction = alders[candidate_name]
+                    office = race_name
+                else:
+                    race_fields = parse_race_name(race_name)
+                    jurisdiction = race_fields['jurisdiction']
+                    office = race_fields['office']
+                # Standard naming convention for jurisdicitons
+                if jurisdiction in standardized_names:
+                    jurisdiction = standardized_names[jurisdiction]
+                if office in standardized_names:
+                    office = standardized_names[office]
                 processed_data.append({
                     "Jurisdiction": jurisdiction,
                     "Office": office,
@@ -266,6 +424,12 @@ def process_html_files(directory_path):
                 combined_list.extend(yearly_list)
     except Exception as e:
         print(f"An error occurred: {e}")
+    # Now do 2024
+    file_path = "cleaned_election_results.txt"
+    print(f"Processing file: {file_path}")
+    yearly_list = process_2024_election_data(file_path, output_file)
+    combined_list.extend(yearly_list)
+
     with open("all_years.csv", 'w', newline='') as csvfile:
         fieldnames = [
             "Jurisdiction", "Office",
